05bf96fc6c875d03a2218e1fa8c97f9fbc9c03d5
digiholic <digikun@gmail.com>
2015-09-22 09:32:36 -0600

    Fixed the titles of the pages

--- start.php
+++ start.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>BattleQuest Dungeon Player</title>
  
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
